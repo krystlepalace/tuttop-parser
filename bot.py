a5b84e9989a194af4d7232b2906392ef0919f8ef
kryslepalace <fubuke993@gmail.com>
2022-11-19 21:17:19 +0300

    some fixes

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import asyncio
 
 
-bot = Bot(token='')
+bot = Bot(token='5804487703:AAExSwZGSWAQB2g1ty2ar03Qez1kdAMUSdk')
 dp = Dispatcher(bot=bot)
 
 @dp.message_handler(commands=['start'])
@@ -13,8 +13,8 @@ async def send_updates(message: Message):
     while True:
         now = datetime.now()
         MSG = '\n\n'.join(f'{name} — {link}' for name, link in get_updates().items())
-        await message.answer(text='Последние обновления:')
-        await message.reply(text=MSG)
+        await message.reply(text='Последние обновления:')
+        await message.answer(text=MSG)
 
         sleeping_time = (3600 * (24 - (now.hour - 6))) if now.hour >= 6 else (3600 * (24 + (6 - now.hour)))
         await asyncio.sleep(delay=sleeping_time)
