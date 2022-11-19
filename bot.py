2be76aa0e2877e5e6b2102bf67572181f6a4b2e9
kryslepalace <fubuke993@gmail.com>
2022-11-19 19:53:18 +0300

    some refactor

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 from aiogram import Bot, Dispatcher, executor
 from aiogram.types import Message
 from parsing import get_updates
-import asyncio
 from datetime import datetime
+import asyncio
 
 
 bot = Bot(token='')
@@ -12,8 +12,10 @@ dp = Dispatcher(bot=bot)
 async def send_updates(message: Message):
     while True:
         now = datetime.now()
-        MSG = '\n'.join(f'{name} — {link}' for name, link in get_updates().items())
-        await bot.send_message(chat_id=message.from_user.id, text=MSG)
+        MSG = '\n\n'.join(f'{name} — {link}' for name, link in get_updates().items())
+        await message.answer(text='Последние обновления:')
+        await message.reply(text=MSG)
+
         sleeping_time = (3600 * (24 - (now.hour - 6))) if now.hour >= 6 else (3600 * (24 + (6 - now.hour)))
         await asyncio.sleep(delay=sleeping_time)
 
