94ef082dce935fff1ebe07d2553516c1a3300919
Ilya <fubuke993@gmail.com>
2022-11-19 18:37:46 +0300

    sleeping time amount fix

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ async def send_updates(message: Message):
         now = datetime.now()
         MSG = '\n'.join(f'{name} — {link}' for name, link in get_updates().items())
         await bot.send_message(chat_id=message.from_user.id, text=MSG)
-        sleeping_time = (3600 * (24 - (now.hour - 6))) if now.hour >= 6 else (6 - now.hour)
+        sleeping_time = (3600 * (24 - (now.hour - 6))) if now.hour >= 6 else (3600 * (24 + (6 - now.hour)))
         await asyncio.sleep(delay=sleeping_time)
 
 if __name__ == '__main__':
