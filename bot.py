42e61b76c7ad9a8d58cdfe439ca1ae6f38a11680
Ilya <fubuke993@gmail.com>
2022-11-19 18:19:39 +0300

    fix for async sleep

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ async def send_updates(message: Message):
         now = datetime.now()
         MSG = '\n'.join(f'{name} — {link}' for name, link in get_updates().items())
         await bot.send_message(chat_id=message.from_user.id, text=MSG)
-        sleeping_time = 24 - (now.hour - 6)
+        sleeping_time = 3600 * (24 - (now.hour - 6))
         await asyncio.sleep(delay=sleeping_time)
 
 if __name__ == '__main__':
